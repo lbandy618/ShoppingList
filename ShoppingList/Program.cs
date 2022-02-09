8ef15bf5332cf1bb9bf3f27b5562cb6ad68f2e91
lbandy618 <97638884+lbandy618@users.noreply.github.com>
2022-02-09 10:07:05 -0500

    adding stuff and being rushed

--- ShoppingList/Program.cs
+++ ShoppingList/Program.cs
@@ -21,7 +21,7 @@ namespace ShoppingList
             };
             bool runProgram = true;
             // shopping list
-            List<string> order = new List<string>();
+            List<string> cart = new List<string>();
             //intro message
             Console.WriteLine("Welcome to my shop!");
             Console.WriteLine();
@@ -30,22 +30,45 @@ namespace ShoppingList
             {
                 Console.WriteLine(String.Format("{0,-15}{1,15}", "Item", "Price"));
                 Console.WriteLine("==================================");
-
-                foreach (KeyValuePair<string, double> kvp in menu)
+                //Clean up console after every item
+                Console.Clear();
+                foreach (KeyValuePair<string, double> item in menu.OrderBy(item => item.Value))
                 {
-                    Console.WriteLine(String.Format("{0,-15}{1,15}", kvp.Key, kvp.Value));
+                    Console.WriteLine(String.Format("{0,-15}{1,15}", item.Key, item.Value));
                 }
                 // accepting user input
                 Console.WriteLine();
                 while (runProgram)
                 {
-                    Console.Write("What item would you like to order?: ");
+                    Console.Write("What item would you like to order? : ");
                     string menuChoice = Console.ReadLine().ToLower().Trim();
-                    order.Add(menuChoice);
 
+                    //checks if exists
                     if (menu.ContainsKey(menuChoice))
                     {
                         Console.WriteLine($"Adding {menuChoice} to the cart at {menu[menuChoice]}");
+                        cart.Add(menuChoice);
+                        //asks if continue
+                        while (runProgram)
+                        {
+                            Console.WriteLine("Would you like to continue shopping? y/n");
+                            string loopCheck = Console.ReadLine();
+                            if (loopCheck.ToLower().Trim() == "y")
+                            {
+                                runProgram = true;
+                                break;
+                            }
+                            else if (loopCheck.ToLower().Trim() == "n")
+                            {
+                                runProgram = false;
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("That was not a valid response");
+                                Console.WriteLine();
+                            }
+                        }
                         Console.WriteLine();
                     }
                     else
@@ -54,7 +77,35 @@ namespace ShoppingList
                         Console.WriteLine("Invalid input entered.");
                         break;
                     }
+
                 }
+
+                //display cart
+                Console.WriteLine();
+                Console.WriteLine("This is your order: ");
+                double total = 0;
+                //highest price
+                string highestName = "lotto ticket";
+                //lowest price
+                string lowestName = "banana";
+
+                foreach (string item in cart)
+                {
+                    Console.WriteLine(string.Format("{0.-15} {1,15}", item, menu[item]));
+                    total += menu[item];
+                    if (menu[item] >= menu[highestName])
+                    {
+                        highestName = item;
+                    }
+                    if (menu[item] <= menu[lowestName])
+                    {
+                        lowestName = item;
+                    }
+                }
+                Console.WriteLine($"Average price per item in order was {Math.Round(total / cart.Count, 2)}");
+                Console.WriteLine($"The highest priced item was {highestName}");
+                Console.WriteLine($"The lowest price item was {lowestName}");
+
                 while (true)
                 {
                     Console.WriteLine();
